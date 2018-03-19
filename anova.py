2dfe0c297a02196a85c7d2ca02f1e6f20aa22822
Johan <jl.westin@hotmail.com>
2018-03-19 21:24:06 +0100

    generalized ANOVA for higher order interactions

--- anova.py
+++ anova.py
@@ -2,6 +2,7 @@ import numpy as np
 from math import log
 
 from factorial_inputs import bin_fixed_length
+from y_functions import all_choices
 
 def n_params(Y):
     return int(round(log(len(Y), 2)))
@@ -10,37 +11,74 @@ def S_T(Y):
     y_bar = sum(Y)/float(len(Y))
     return sum([(y - y_bar)**2. for y in Y])
 
-def S_i(Y, i):
+def S_minmax(Y, indices, bit):
     N = len(Y)
     n = n_params(Y)
-    
-    y_lo, y_hi = 0., 0.
+    S = 0.
 
     for k in range(N):
         bitstring = bin_fixed_length(k, n)
-        
-        if bitstring[i] == "1":
-            y_hi += Y[k]
-        elif bitstring[i] == "0":
-            y_lo += Y[k]
-        else:
-            raise ValueError("Invalid digit in bit string.")
+        if not bit in [int(bitstring[i]) for i in indices]:
+            S += Y[k]
 
-    return ((y_hi - y_lo)**2. )/float(N)
+    return S
 
-def S_i_set(Y):
-    return [S_i(Y, i) for i in range(n_params(Y))]
+def S_low(Y, indices):
+    return S_minmax(Y, indices, 1)
 
-def S_E(Y):
-    return S_T(Y) - sum(S_i_set(Y))
+def S_high(Y, indices):
+    return S_minmax(Y, indices, 0)
 
-def ANOVA(Y, display=True):
-    P = np.array(S_i_set(Y))/S_T(Y)
+def S_i(Y, indices):
+    factor = 1. / len(Y)
+    return factor * ((S_high(Y, indices) - S_low(Y, indices))**2. )
+
+def ANOVA_partial(Y, order, display=False):
+    index_inputs = all_choices(range(5), order)
+    interaction_set = []
+    ST = S_T(Y)
+    
+    for indices in index_inputs:
+        interaction_set.append(S_i(Y, indices)/ST)
 
     if display:
-        pct = [round(p*100., 2) for p in P]
-        for n in range(n_params(Y)):
-            print " P_" + str(n), "=", pct[n], "%"
+        pct = [round(S*100., 2) for S in interaction_set]
+        for i in range(len(pct)):
+            P_name = " P_"
+            for index in index_inputs[i]:
+                P_name = P_name + str(index)
+            print P_name, "=", pct[i], "%"
         print
 
-    return P
+    return index_inputs, interaction_set
+
+def interaction_contribution(Y, order):
+    return sum(ANOVA_partial(Y, order, display=False)[1])
+
+def contribution_summary(Y, max_order=-1, display=True):
+    if max_order == -1:
+        max_order = n_params(Y)
+
+    contributions = [interaction_contribution(Y, order) for order in range(1, max_order+1)]
+
+    if display:
+        order = 1
+        for contrib in contributions:
+            print " Order", order, "contribution:", round(contrib*100., 2), "%"
+            order += 1
+
+    return contributions
+
+def ANOVA_full(Y, display=False):
+    index_inputs = []
+    interaction_set = []
+
+    for order in range(1, n_params(Y) + 1):
+        i, S_i = ANOVA_partial(Y, order, display=display)
+        index_inputs = index_inputs + i
+        interaction_set = interaction_set + S_i
+
+    return index_inputs, interaction_set
+
+def S_E(Y):
+    return S_T(Y) - sum(ANOVA_full(Y, display=False)[1])
--- hw4_outputs.py
+++ hw4_outputs.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from assignment_number import asmt
 from factorial_inputs import evaluate_factorial_design
-from anova import ANOVA
+from anova import ANOVA_full, contribution_summary
 from y_functions import y1, y2, y2_noisy
 
 if asmt == 1:
@@ -27,9 +27,10 @@ if asmt == 3:
                     [ 0.9, 3.1]]
 
 out_y1       = evaluate_factorial_design(param_limits, y1)
-out_y2       = evaluate_factorial_design(param_limits, y2)
-out_y2_noisy = evaluate_factorial_design(param_limits, y2_noisy)
+##out_y2       = evaluate_factorial_design(param_limits, y2)
+##out_y2_noisy = evaluate_factorial_design(param_limits, y2_noisy)
 
-var_y1       = ANOVA(out_y1)
-var_y2       = ANOVA(out_y2)
-var_y2_noisy = ANOVA(out_y2_noisy)
+cnt_y1 = contribution_summary(out_y1)
+##all_indices, interactions       = ANOVA_full(out_y1)
+##var_y2       = ANOVA_full(out_y2)
+##var_y2_noisy = ANOVA_full(out_y2_noisy)
